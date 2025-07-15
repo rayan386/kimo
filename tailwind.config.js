efdfb8ab1fca34e66ee6398004cb89630378b780
globex <onlydanzo87@gmail.com>
2025-07-15 13:57:53 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF6D00",
+        secondary: "#f5f5f5",
+        dark: "#333333"
+      }
+    }
+  },
+  plugins: []
+}
